1e93454a2d225f0241ad642f7f77396b3ca29484
Zhi-Hao Lin <cl121@illinois.edu>
2024-04-24 14:10:17 -0500

    tried bakedsdf-mlp

--- zhi-hao/scripts/scannetpp/run.sh
+++ zhi-hao/scripts/scannetpp/run.sh
@@ -1,30 +1,44 @@
 # # bakedsdf
-# python scripts/train.py bakedsdf --vis wandb \
-#     --output-dir outputs/scannetpp --experiment-name 240422_office3_bakedsdf-non-normalized-contraction-inf \
-#     --trainer.steps-per-eval-image 5000 --trainer.steps-per-eval-all-images 50000 \
-#     --trainer.max-num-iterations 250001 --trainer.steps-per-eval-batch 5000 \
-#     --pipeline.model.sdf-field.bias 1.5 --pipeline.model.sdf-field.inside-outside True \
-#     --pipeline.model.eikonal-loss-mult 0.01 --pipeline.model.num-neus-samples-per-ray 24 \
-#     --machine.num-gpus 1 \
-#     --pipeline.model.mono-normal-loss-mult 0.1 \
-#     panoptic-data \
-#     --data ../datasets/scannetpp/data/4a1a3a7dc5/psdf/ \
-#     --panoptic_data False --mono_normal_data True --panoptic_segment False \
-#     --orientation-method none --center-poses False --auto-scale-poses False
+python scripts/train.py bakedsdf --vis wandb \
+    --output-dir outputs/scannetpp --experiment-name 240422_office3_bakedsdf-non-normalized-contraction-inf \
+    --trainer.steps-per-eval-image 5000 --trainer.steps-per-eval-all-images 50000 \
+    --trainer.max-num-iterations 250001 --trainer.steps-per-eval-batch 5000 \
+    --pipeline.model.sdf-field.bias 1.5 --pipeline.model.sdf-field.inside-outside True \
+    --pipeline.model.eikonal-loss-mult 0.01 --pipeline.model.num-neus-samples-per-ray 24 \
+    --machine.num-gpus 1 \
+    --pipeline.model.mono-normal-loss-mult 0.1 \
+    panoptic-data \
+    --data ../../../datasets/scannetpp/data/4a1a3a7dc5/psdf/ \
+    --panoptic_data False --mono_normal_data True --panoptic_segment False \
+    --orientation-method none --center-poses False --auto-scale-poses False
+
+# # bakedsdf-mlp
+python scripts/train.py bakedsdf-mlp --vis wandb \
+    --output-dir outputs/scannetpp --experiment-name 240422_office3_bakedsdf-mlp-non-normalized \
+    --trainer.steps-per-eval-image 5000 --trainer.steps-per-eval-all-images 50000 \
+    --trainer.max-num-iterations 250001 --trainer.steps-per-eval-batch 5000 \
+    --pipeline.model.sdf-field.bias 1.5 --pipeline.model.sdf-field.inside-outside True \
+    --pipeline.model.eikonal-loss-mult 0.01 --pipeline.model.num-neus-samples-per-ray 24 \
+    --machine.num-gpus 1 \
+    --pipeline.model.mono-normal-loss-mult 0.1 \
+    panoptic-data \
+    --data ../../../datasets/scannetpp/data/4a1a3a7dc5/psdf/ \
+    --panoptic_data False --mono_normal_data True --panoptic_segment False \
+    --orientation-method none --center-poses False --auto-scale-poses False
 
 # # monosdf
-# python scripts/train.py monosdf --vis wandb \
-#     --output-dir outputs/scannetpp --experiment-name 240422_office3_monosdf-non-normalized \
-#     --trainer.steps-per-eval-image 5000 --trainer.steps-per-eval-all-images 50000 \
-#     --trainer.max-num-iterations 250001 --trainer.steps-per-eval-batch 5000 \
-#     --pipeline.model.sdf-field.bias 1.5 --pipeline.model.sdf-field.inside-outside True \
-#     --pipeline.model.eikonal-loss-mult 0.01 \
-#     --machine.num-gpus 1 \
-#     --pipeline.model.mono-normal-loss-mult 0.1 \
-#     panoptic-data \
-#     --data ../datasets/scannetpp/data/4a1a3a7dc5/psdf/ \
-#     --panoptic_data False --mono_normal_data True --panoptic_segment False \
-#     --orientation-method none --center-poses False --auto-scale-poses False
+python scripts/train.py monosdf --vis wandb \
+    --output-dir outputs/scannetpp --experiment-name 240422_office3_monosdf-non-normalized \
+    --trainer.steps-per-eval-image 5000 --trainer.steps-per-eval-all-images 50000 \
+    --trainer.max-num-iterations 250001 --trainer.steps-per-eval-batch 5000 \
+    --pipeline.model.sdf-field.bias 1.5 --pipeline.model.sdf-field.inside-outside True \
+    --pipeline.model.eikonal-loss-mult 0.01 \
+    --machine.num-gpus 1 \
+    --pipeline.model.mono-normal-loss-mult 0.1 \
+    panoptic-data \
+    --data ../../../datasets/scannetpp/data/4a1a3a7dc5/psdf/ \
+    --panoptic_data False --mono_normal_data True --panoptic_segment False \
+    --orientation-method none --center-poses False --auto-scale-poses False
 
 # neuralangelo
 python scripts/train.py neuralangelo --vis wandb \
@@ -36,11 +50,6 @@ python scripts/train.py neuralangelo --vis wandb \
     --machine.num-gpus 1 \
     --pipeline.model.mono-normal-loss-mult 0.1 \
     panoptic-data \
-    --data ../datasets/scannetpp/data/4a1a3a7dc5/psdf/ \
+    --data ../../../datasets/scannetpp/data/4a1a3a7dc5/psdf/ \
     --panoptic_data False --mono_normal_data True --panoptic_segment False \
     --orientation-method none --center-poses False --auto-scale-poses False
-
-# python scripts/extract_mesh.py --load-config outputs/scannetpp/240417_office3_bakedsdf-hongchi-merge-delta/bakedsdf/2024-04-17_231030/config.yml \
-#     --output-path outputs/scannetpp/240417_office3_bakedsdf-hongchi-merge-delta/bakedsdf/2024-04-17_231030/mesh.ply \
-#     --bounding-box-min -2.0 -2.0 -2.0 --bounding-box-max 2.0 2.0 2.0 \
-#     --resolution 2048 --marching_cube_threshold 0.001 --create_visibility_mask True --simplify-mesh True
